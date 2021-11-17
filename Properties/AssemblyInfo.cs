f82481840b2d29889437f78da30f5bf3639c9a64
DarkSwitchPro <59634615+DarkSwitchPro@users.noreply.github.com>
2021-11-16 22:34:16 -0500

    Final

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using MelonLoader;
+
+[assembly: AssemblyTitle(LightAmmoPouch.BuildInfo.Name)]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(LightAmmoPouch.BuildInfo.Company)]
+[assembly: AssemblyProduct(LightAmmoPouch.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + LightAmmoPouch.BuildInfo.Author)]
+[assembly: AssemblyTrademark(LightAmmoPouch.BuildInfo.Company)]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+//[assembly: Guid("")]
+[assembly: AssemblyVersion(LightAmmoPouch.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(LightAmmoPouch.BuildInfo.Version)]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: MelonInfo(typeof(LightAmmoPouch.LightAmmoPouch), LightAmmoPouch.BuildInfo.Name, LightAmmoPouch.BuildInfo.Version, LightAmmoPouch.BuildInfo.Author, LightAmmoPouch.BuildInfo.DownloadLink)]
+[assembly: MelonGame(null, null)]
